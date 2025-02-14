a56d208ec52fdf313e18e17c345699846d7ff8dc
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-02-14 14:16:54 -0500

    Rebuild

--- browser/ts_react_spa/vite.config.ts
+++ browser/ts_react_spa/vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/browser/ts_react_spa/dist/"
+  // base: "/browser/ts_react_spa/dist/"
+  base: "/client-js-examples/ts_react_spa/"
+  // base: "/ts_react_spa/"
 })

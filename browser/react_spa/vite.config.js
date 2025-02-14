4aa12c3cbef1e235791e1e3393c2af2070b2b8f9
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-02-14 17:43:05 -0500

    Fix the build

--- browser/react_spa/vite.config.js
+++ browser/react_spa/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/client-js-examples/react_spa/"
+  // base: "/client-js-examples/react_spa/"
+  base: "./"
 })
